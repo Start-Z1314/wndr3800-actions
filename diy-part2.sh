09fd080c1f60481b40f6747a7b4f71d7265b30d8
Start-Z1314 <zqk003@gmail.com>
2026-03-12 22:22:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,141 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+set -e
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo "开始执行 diy-part2.sh (旧版源码)..."
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 1. 强制修改默认主题为 Argon（检查路径是否存在）
+if [ -f "feeds/luci/collections/luci/Makefile" ]; then
+    sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+    echo "✓ 默认主题已修改为 Argon"
+fi
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 2. WiFi 物理修改（强制开启）
+if [ -f "package/kernel/mac80211/files/lib/wifi/mac80211.sh" ]; then
+    sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+    sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio${devidx}.ssid=5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+    echo "✓ WiFi 已强制开启，默认 SSID 改为 5G"
+fi
+
+# 3. 创建 UCI 默认配置脚本（包含你所有预设）
+mkdir -p package/base-files/files/etc/uci-defaults
+cat > package/base-files/files/etc/uci-defaults/99-init-settings <<'EOF'
+#!/bin/sh
+# 系统设置
+uci set system.@system[0].hostname='OpenWrt'
+uci commit system
+
+# WiFi 设置
+uci set wireless.radio0.country='US'
+uci set wireless.radio1.country='US'
+uci set wireless.radio0.channel='4'
+uci set wireless.radio1.channel='149'
+uci set wireless.radio0.txpower='20'
+uci set wireless.radio1.txpower='20'
+uci set wireless.@wifi-iface[0].ssid='5G'
+uci set wireless.@wifi-iface[0].key='zld74502'
+uci set wireless.@wifi-iface[0].encryption='psk2'
+uci set wireless.@wifi-iface[1].ssid='5G'
+uci set wireless.@wifi-iface[1].key='zld74502'
+uci set wireless.@wifi-iface[1].encryption='psk2'
+uci commit wireless
+wifi up
+
+# Turbo ACC 加速
+uci set turboacc.config.sfe_flow='1'
+uci set turboacc.config.dns_cache='1'
+uci commit turboacc
+
+# zRAM 交换内存
+uci set zram.config.enabled='1'
+uci set zram.config.zram_size='64'
+uci commit zram
+
+# CPU 频率管理（锁定性能模式，超频至800MHz）
+uci set cpufreq.default.governor='performance'
+uci set cpufreq.default.min_freq='800000'
+uci set cpufreq.default.max_freq='800000'
+uci commit cpufreq
+
+# SSR Plus+ 代理订阅预设
+uci set ssrplus.@global[0].global_mode='1'
+uci set ssrplus.@global[0].dns_hijack='1'
+uci set ssrplus.@global[0].chinadns_ng_enable='1'
+uci set ssrplus.@global[0].chinadns_ng_china_dns='114.114.114.114,223.5.5.5'
+uci set ssrplus.@global[0].chinadns_ng_trust_dns='8.8.8.8'
+uci set ssrplus.@global[0].udp_relay_server='1'
+uci set ssrplus.@global[0].tcp_fast_open='1'
+uci set ssrplus.@subscribe[0].enabled='1'
+uci set ssrplus.@subscribe[0].subtype='0'
+uci set ssrplus.@subscribe[0].cron_time='0 3 * * *'
+uci set ssrplus.@subscribe[0].sub_url='https://example.com/your-subscribe-url'   # 请自行修改订阅链接
+uci commit ssrplus
+
+# UPnP 端口转发（开启）
+uci set upnpd.config.enabled='1'
+uci set upnpd.config.upnp_lease_file='/var/upnp.leases'
+uci set upnpd.config.secure_mode='1'
+uci set upnpd.config.log_output='1'
+uci commit upnpd
+
+# 石像鬼 QoS 规则（完全按你的预设）
+uci set qos.gargoyle.enabled='1'
+uci set qos.gargoyle.wan_iface='wan'
+uci set qos.gargoyle.uplink_smart='1'
+uci set qos.gargoyle.downlink_smart='1'
+uci set qos.gargoyle.ack_priority='1'
+uci set qos.gargoyle.default_class='4'
+DOWNLOAD_TOTAL="102400"      # 总下载带宽 100Mbps
+UPLOAD_TOTAL="10240"         # 总上传带宽 10Mbps
+uci set qos.gargoyle.download_bandwidth="${DOWNLOAD_TOTAL}"
+uci set qos.gargoyle.upload_bandwidth="${UPLOAD_TOTAL}"
+
+uci set qos.class_1.percent_min='35'
+uci set qos.class_1.percent_max='95'
+uci set qos.class_2.percent_min='20'
+uci set qos.class_2.percent_max='80'
+uci set qos.class_3.percent_min='15'
+uci set qos.class_3.percent_max='60'
+uci set qos.class_4.percent_min='5'
+uci set qos.class_4.percent_max='40'
+
+# 添加 QoS 规则
+uci add qos rule
+uci set qos.@rule[-1].name='Game'
+uci set qos.@rule[-1].priority='Highest'
+uci set qos.@rule[-1].ports='7000-8000 27015-27030 3074 3478-3479 5060 5062 6000-6200 10000-20000 30000-40000'
+
+uci add qos rule
+uci set qos.@rule[-1].name='Web_HTTP'
+uci set qos.@rule[-1].priority='High'
+uci set qos.@rule[-1].ports='80 443 8080 8443'
+
+uci add qos rule
+uci set qos.@rule[-1].name='Web_Large'
+uci set qos.@rule[-1].priority='Normal'
+uci set qos.@rule[-1].ports='80 443 8080 8443'
+uci set qos.@rule[-1].threshold='5120'
+uci set qos.@rule[-1].threshold_unit='kb'
+
+uci add qos rule
+uci set qos.@rule[-1].name='Video'
+uci set qos.@rule[-1].priority='High'
+uci set qos.@rule[-1].ports='80 443 1935 5223 8000-9000 10000-20000'
+
+uci add qos rule
+uci set qos.@rule[-1].name='Chat'
+uci set qos.@rule[-1].priority='High'
+uci set qos.@rule[-1].ports='53 80 443 5222 5223 5228 5229 5230 8000-8010 8080 8443'
+
+uci add qos rule
+uci set qos.@rule[-1].name='Download'
+uci set qos.@rule[-1].priority='Normal'
+uci set qos.@rule[-1].ports='6881-6889 1863 5190 5000-5010 8080 10000-20000'
+
+uci commit qos
+
+exit 0
+EOF
+
+chmod +x package/base-files/files/etc/uci-defaults/99-init-settings
+echo "✓ UCI 默认配置脚本已创建"
+echo "diy-part2.sh 执行完成"
